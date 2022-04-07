3fbc122b726423902fc2f798384160a7099eb0d6
Tobias Ruck <ruck.tobias@gmail.com>
2022-04-07 15:37:17 +0200

    Add `setup_bitcoind_coins`
    Simplifies setting up coins for testing if there's already a bitcoind instance running.

--- bitcoinsuite-test-utils-blockchain/src/lib.rs
+++ bitcoinsuite-test-utils-blockchain/src/lib.rs
@@ -4,7 +4,10 @@ use bitcoinsuite_bchd_grpc::{
     bchd_grpc::GetBlockchainInfoRequest,
     test_instance::{BchdTestConf, BchdTestInstance},
 };
-use bitcoinsuite_bitcoind::instance::{BitcoindChain, BitcoindConf, BitcoindInstance};
+use bitcoinsuite_bitcoind::{
+    cli::BitcoinCli,
+    instance::{BitcoindChain, BitcoindConf, BitcoindInstance},
+};
 use bitcoinsuite_core::{
     BitcoinCode, Hashed, Network, OutPoint, Script, SequenceNo, Sha256d, TxInput, TxOutput,
     UnhashedTx,
@@ -32,19 +35,13 @@ pub async fn setup_bch_chain(
     setup_chain(Network::BCH, bch_conf, num_generated_utxos, redeem_script).await
 }
 
-pub async fn setup_chain(
+pub fn setup_bitcoind_coins(
+    bitcoind: &BitcoinCli,
     network: Network,
-    bitcoind_conf: BitcoindConf,
     num_generated_utxos: i32,
-    redeem_script: &Script,
-) -> Result<(BitcoindInstance, BchdTestInstance, Vec<(OutPoint, i64)>)> {
-    let mut bitcoind = BitcoindInstance::setup(bitcoind_conf)?;
-    bitcoind.wait_for_ready()?;
-    let bchd_conf = BchdTestConf::from_env(bitcoind.p2p_port(), vec![])?;
-    let mut bchd = BchdTestInstance::setup(bchd_conf).await?;
-    let script_hash = redeem_script.to_p2sh();
-    let address = bitcoind.cmd_json("decodescript", &[&redeem_script.hex()])?;
-    let address = address["p2sh"].as_str().unwrap();
+    address: &str,
+    script_hex: &str,
+) -> Result<Vec<(OutPoint, i64)>> {
     let blocks = bitcoind.cmd_json(
         "generatetoaddress",
         &[&num_generated_utxos.to_string(), address],
@@ -55,7 +52,7 @@ pub async fn setup_chain(
         let block = bitcoind.cmd_json("getblock", &[block.as_str().unwrap(), "2"])?;
         let tx = &block["tx"][0];
         for (out_idx, output) in tx["vout"].members().enumerate() {
-            if output["scriptPubKey"]["hex"].as_str().unwrap() == script_hash.hex() {
+            if output["scriptPubKey"]["hex"].as_str().unwrap() == script_hex {
                 let amount = output["value"]
                     .as_fixed_point_i64(network.coin_decimals() as u16)
                     .unwrap();
@@ -69,6 +66,29 @@ pub async fn setup_chain(
             }
         }
     }
+    Ok(utxos)
+}
+
+pub async fn setup_chain(
+    network: Network,
+    bitcoind_conf: BitcoindConf,
+    num_generated_utxos: i32,
+    redeem_script: &Script,
+) -> Result<(BitcoindInstance, BchdTestInstance, Vec<(OutPoint, i64)>)> {
+    let mut bitcoind = BitcoindInstance::setup(bitcoind_conf)?;
+    bitcoind.wait_for_ready()?;
+    let bchd_conf = BchdTestConf::from_env(bitcoind.p2p_port(), vec![])?;
+    let mut bchd = BchdTestInstance::setup(bchd_conf).await?;
+    let script_hash = redeem_script.to_p2sh();
+    let address = bitcoind.cmd_json("decodescript", &[&redeem_script.hex()])?;
+    let address = address["p2sh"].as_str().unwrap();
+    let utxos = setup_bitcoind_coins(
+        bitcoind.cli(),
+        network,
+        num_generated_utxos,
+        address,
+        &script_hash.hex(),
+    )?;
     for attempt in 0..100 {
         if attempt == 100 {
             panic!("Timeout waiting for blocks");
