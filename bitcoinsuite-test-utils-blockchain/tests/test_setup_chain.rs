cbc58466dd7fce785d3756c107e928b7fed948cb
Tobias <ruck.tobias@gmail.com>
2022-06-13 15:08:30 +0200

    Remove `bitcoinsuite-bchd-grpc` (#22)
    BCHD isn't that well maintained, and most features are being in the process of replaced with Chronik.

--- bitcoinsuite-test-utils-blockchain/tests/test_setup_chain.rs
+++ bitcoinsuite-test-utils-blockchain/tests/test_setup_chain.rs
@@ -1,4 +1,3 @@
-use bitcoinsuite_bchd_grpc::bchd_grpc::GetBlockchainInfoRequest;
 use bitcoinsuite_core::Script;
 use bitcoinsuite_error::Result;
 use bitcoinsuite_test_utils_blockchain::{setup_bch_chain, setup_xec_chain};
@@ -6,14 +5,8 @@ use bitcoinsuite_test_utils_blockchain::{setup_bch_chain, setup_xec_chain};
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn test_setup_xec_chain() -> Result<()> {
     let redeem_script = Script::from_slice(&[0x51]);
-    let (bitcoind, mut bchd, utxos) = setup_xec_chain(10, &redeem_script).await?;
+    let (bitcoind, utxos) = setup_xec_chain(10, &redeem_script).await?;
     assert_eq!(bitcoind.cmd_string("getblockcount", &[])?, "110");
-    let response = bchd
-        .client()
-        .get_blockchain_info(GetBlockchainInfoRequest {})
-        .await?
-        .into_inner();
-    assert_eq!(response.best_height, 110);
     assert_eq!(utxos.len(), 10);
     Ok(())
 }
@@ -21,14 +14,8 @@ async fn test_setup_xec_chain() -> Result<()> {
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn test_setup_bch_chain() -> Result<()> {
     let redeem_script = Script::from_slice(&[0x51]);
-    let (bitcoind, mut bchd, utxos) = setup_bch_chain(10, &redeem_script).await?;
+    let (bitcoind, utxos) = setup_bch_chain(10, &redeem_script).await?;
     assert_eq!(bitcoind.cmd_string("getblockcount", &[])?, "110");
-    let response = bchd
-        .client()
-        .get_blockchain_info(GetBlockchainInfoRequest {})
-        .await?
-        .into_inner();
-    assert_eq!(response.best_height, 110);
     assert_eq!(utxos.len(), 10);
     Ok(())
 }
