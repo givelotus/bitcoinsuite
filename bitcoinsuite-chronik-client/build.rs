22d0272480d412abe0bb067a7fa18f72277a748f
Tobias Ruck <ruck.tobias@gmail.com>
2023-06-08 19:53:18 +0200

    Add `rerun-if-changed` to build.rs in bitcoinsuite-chronik-client

--- bitcoinsuite-chronik-client/build.rs
+++ bitcoinsuite-chronik-client/build.rs
@@ -2,5 +2,6 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     prost_build::compile_protos(&["proto/chronik.proto"], &["proto/"])?;
+    println!("cargo:rerun-if-changed=proto/chronik.proto");
     Ok(())
 }
